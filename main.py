259bfd33b950890447af4f0dc27b7d2c4c181961
[Radin afshar] <[afshar1381@gmail.com]>
2022-07-12 12:16:09 +0300

    added module 4 fully working & added to the main script

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import cv2 
 import os
 import random
-import module1
-import module2
-import module3
+from module1 import *
+from module2 import *
+from module3 import *
+from module4 import *
 import numpy as np
 import tensorflow as tf
 from matplotlib import pyplot as plt 
@@ -12,7 +13,7 @@ from tensorflow.python.keras.layers import Layer, Conv2D, Dense, MaxPooling2D, I
 
 
 if os.path.isdir('data') is False:
-   module1.setup1()
+   setup1()
 
 
-module2.collect_positive_anchor()
+collect_positive_anchor()
