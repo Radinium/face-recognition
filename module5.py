9cff38cb4a630b2a5a73059d24452408699df2b1
[Radin afshar] <[afshar1381@gmail.com]>
2022-07-12 12:33:31 +0300

    module 5 fully working

---
+++ module5.py
@@ -0,0 +1,65 @@
+from module4 import *
+from module3 import *
+import tensorflow as tf
+from tensorflow.keras.metrics import Precision, Recall
+
+
+binary_cross_loss = tf.losses.BinaryCrossentropy()
+opt = tf.keras.optimizers.Adam(1e-4) # 0.0001
+
+
+checkpoint_dir = './training_checkpoints'
+checkpoint_prefix = os.path.join(checkpoint_dir, 'ckpt')
+checkpoint = tf.train.Checkpoint(opt=opt, siamese_model=siamese_model)
+
+@tf.function
+def train_step(batch):
+    
+    # Record all of our operations 
+    with tf.GradientTape() as tape:     
+        # Get anchor and positive/negative image
+        X = batch[:2]
+        # Get label
+        y = batch[2]
+        
+        # Forward pass
+        yhat = siamese_model(X, training=True)
+        # Calculate loss
+        loss = binary_cross_loss(y, yhat)
+    print(loss)
+        
+    # Calculate gradients
+    grad = tape.gradient(loss, siamese_model.trainable_variables)
+    
+    # Calculate updated weights and apply to siamese model
+    opt.apply_gradients(zip(grad, siamese_model.trainable_variables))
+        
+    # Return loss
+    return loss
+
+def train(data, EPOCHS):
+    # Loop through epochs
+    for epoch in range(1, EPOCHS+1):
+        print('\n Epoch {}/{}'.format(epoch, EPOCHS))
+        progbar = tf.keras.utils.Progbar(len(data))
+        
+        # Creating a metric object 
+        r = Recall()
+        p = Precision()
+        
+        # Loop through each batch
+        for idx, batch in enumerate(data):
+            # Run train step here
+            loss = train_step(batch)
+            yhat = siamese_model.predict(batch[:2])
+            r.update_state(batch[2], yhat)
+            p.update_state(batch[2], yhat) 
+            progbar.update(idx+1)
+        print(loss.numpy(), r.result().numpy(), p.result().numpy())
+        
+        # Save checkpoints
+        if epoch % 10 == 0: 
+            checkpoint.save(file_prefix=checkpoint_prefix)
+
+EPOCHS = 50
+train(train_data, EPOCHS)
